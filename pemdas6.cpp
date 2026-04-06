714b0d71ba2d9f80ee3c26316ee20277369159dc
ikhwalkh <203380321+ikhwalkh@users.noreply.github.com>
2026-04-06 14:56:28 +0700

    // deklarasi dengan inisialisasi

--- pemdas6.cpp
+++ pemdas6.cpp
@@ -1,2 +1,11 @@
 #include <iostream>
 using namespace std;
+
+int main() {
+    // deklarasi dengan inisialisasi
+    int aku[5] = {10,20,30,40,50};
+    // deklarasi array tanpa inisialisasi
+    int kamu[5];
+    // tampilan data
+    cout << "Data pada index 1 = " << aku[1] << endl;
+}
