bbc4bfc0a4024b7f70e41f8aeb2686961e8e05be
ikhwalkh <203380321+ikhwalkh@users.noreply.github.com>
2026-04-06 15:19:18 +0700

    menampilkan dengan menggunakan perulangan for

--- arrayNew.cpp
+++ arrayNew.cpp
@@ -38,5 +38,10 @@ int main() {
             cout << "Data index ke-" << i << " = ";
             cin >> kamu[1];
         }
-        
+        cout << endl;
+        // menampilkan dengan menggunakan perulangan for
+        for (int i = 0; i <= 4; i++)
+        {
+            cout << "Data ke-" << i+1 << " = " << kamu[i] << endl;
+        }
 }
