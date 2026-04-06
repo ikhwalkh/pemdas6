0bef0efd7febda203d56efaa26ebe5a6e8fc84c2
ikhwalkh <203380321+ikhwalkh@users.noreply.github.com>
2026-04-06 15:10:16 +0700

    menampilkan satu per satu

--- arrayNew.cpp
+++ arrayNew.cpp
@@ -23,4 +23,11 @@ int main() {
     cin >> kamu[3];
     cout << "Data index 4 = ";
     cin >> kamu[4];
+    cout << endl;
+    // menampilkan satu per satu
+    cout << "Data pertama = " << kamu[0] << endl;
+     cout << "Data kedua = " << kamu[1] << endl;
+      cout << "Data ketiga = " << kamu[2] << endl;
+       cout << "Data keempat = " << kamu[3] << endl;
+        cout << "Data kelima = " << kamu[4] << endl;
 }
